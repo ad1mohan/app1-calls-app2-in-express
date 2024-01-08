d6cab9fe7a4e0a7f251d20b50a2b76f3a7b581cb
Aditya Mohan <87567444+ad1mohan@users.noreply.github.com>
2024-01-08 17:32:45 +0530

    Add files via upload

--- app-1/app.js
+++ app-1/app.js
@@ -1,33 +1,27 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-const AWSXRay = require('aws-xray-sdk');
+
 const app = express();
 const port = 3000; 
 
-// app.use(express.static('public'));
-// Use the X-Ray middleware
-app.use(AWSXRay.express.openSegment('App1'));
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'public', 'index.html');
-
-  // Send the HTML file as the response
   res.sendFile(filePath);
 });
 
 app.get('/app-2', async(req, res) => {
     try {
-        const response = await axios.get('http://app-2.service.local:3001');
+        const response = await axios.get('http://localhost:3001');
         res.json(response.data);
       } catch (error) {
         console.error('Error making request:', error);
         res.status(500).send('Internal Server Error');
       }
 });
-
-app.use(AWSXRay.express.closeSegment());
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
--- app-2/app.js
+++ app-2/app.js
@@ -1,20 +1,14 @@
 const express = require('express');
-const AWSXRay = require('aws-xray-sdk');
 const path = require('path');
 const app = express();
 const port = 3001;
 
-// app.use(express.static('public'));
-// Use the X-Ray middleware
-app.use(AWSXRay.express.openSegment('App2'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, 'public', 'index.html');
     res.sendFile(filePath);
 });
-
-// Close the X-Ray segment after the response is sent
-app.use(AWSXRay.express.closeSegment());
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
